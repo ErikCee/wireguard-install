9f38df9ecab459f681f39dee1f1bcea2d6fd00c3
ThpryrChn <benjamin@reelworks.net>
2019-10-08 11:53:43 -0400

    Re-worked the script to add multiple clients, and add clients smarter.

--- addclients.sh
+++ addclients.sh
@@ -1,30 +1,47 @@
 #!/bin/bash
-cd "$HOME" || exit
-SERVER_WG_NIC="wg0"
-read -rp "WireGuard interface name: " -e -i "$SERVER_WG_NIC" SERVER_WG_NIC
-WG_START=3
+if [[ $1 = "" ]] || [ ! -e "$1" ]; then
+	echo "USAGE:	$(basename "$0") <Highest Client Prefered>"
+	exit 1
+else
+	source="$1"
+fi
+# cd "$HOME" || exit
+SERVER_WG_NIC="$(basename "$source")"
+if [ ! -e "/etc/wireguard/${SERVER_WG_NIC%%-*}.conf" ]; then
+	SERVER_WG_NIC="wg0"
+	read -rp "WireGuard interface name: " -e -i "$SERVER_WG_NIC" SERVER_WG_NIC
+else
+	SERVER_WG_NIC=${SERVER_WG_NIC%%-*}
+fi
+WG_START=${source/*-}
+WG_START=$(( ${WG_START%.*} + 1 ))
 read -rp "WireGuard Client aditional IP start: " -e -i "$WG_START" WG_START
-WG_END=13
-read -rp "WireGuard Client aditional IP end: " -e -i "$WG_END" WG_END
+WG_END=$(( WG_START + 5 ))
+read -rp "WireGuard Client aditional IP end(Default makes 5 Configs): " -e -i "$WG_END" WG_END
 if [ -e /etc/wireguard/"$SERVER_WG_NIC".conf ]; then
 	echo "Using /etc/wireguard/$SERVER_WG_NIC.conf"
 else
 	echo "$SERVER_WG_NIC doesn't seem to exist. Here is what is in your /etc/wireguard folder..."
 	ls -1 /etc/wireguard
+	exit 1
 fi
-cp /etc/wireguard/"$SERVER_WG_NIC".conf .
+cp /etc/wireguard/"$SERVER_WG_NIC".conf /etc/wireguard/"$SERVER_WG_NIC.conf-$(date '+%Y-%d-%m %H%M').bak"
 
-ipv6prefix=$(grep Address $HOME/$SERVER_WG_NIC-client.conf | awk '{ print $3 }' | sed -e 's+.*,++' ) ; ipv6prefix="${ipv6prefix%:*}:"
-ipv4prefix=$(grep Address $HOME/$SERVER_WG_NIC-client.conf | awk '{ print $3 }' ) ; ipv4prefix="${ipv4prefix%.*}."
+ipv6prefix=$(grep Address "$source" | awk '{ print $3 }' | sed -e 's+.*,++' ) ; ipv6prefix="${ipv6prefix%:*}:"
+ipv4prefix=$(grep Address "$source" | awk '{ print $3 }' ) ; ipv4prefix="${ipv4prefix%.*}."
+echo "Stopping Wireguard on interface $SERVER_WG_NIC to make changes"
+wg-quick down $SERVER_WG_NIC
 for ((i=WG_START; i<=WG_END; i++)) ; do
 	private=$(wg genkey)
 	public=$(echo "$private" | wg pubkey)
-	sed "2 cPrivateKey = $private" "$SERVER_WG_NIC"-client.conf > "$SERVER_WG_NIC"-client-$i.conf || exit
-	sed -i "3 cAddress = $ipv4prefix$i\/24,$ipv6prefix$i\/64" "$SERVER_WG_NIC"-client-$i.conf
+	sed "2 cPrivateKey = $private" "$source" > "${source%-*}"-$i.conf || exit 1
+	sed -i "3 cAddress = $ipv4prefix$i\/24,$ipv6prefix$i\/64" "${source%-*}"-$i.conf
 	echo "[Peer]
 PublicKey = $public
 AllowedIPs = $ipv4prefix$i/32,$ipv6prefix$i/128
 $(grep PresharedKey /etc/wireguard/"$SERVER_WG_NIC".conf | head -1)
-" >> "$SERVER_WG_NIC".conf
+" >> /etc/wireguard/"$SERVER_WG_NIC".conf
 done
-echo "Check the $SERVER_WG_NIC.conf at $HOME/. If everything looks right, replace /etc/wireguard/$SERVER_WG_NIC.conf with it. "
+echo "Starting WireGuard on interface $SERVER_WG_NIC. The new configs are ready. Your original interface config was backed up.
+"
+wg-quick up $SERVER_WG_NIC
--- wireguard-install.sh
+++ wireguard-install.sh
@@ -58,22 +58,30 @@ read -rp "Server's WireGuard IPv6 " -e -i "$SERVER_WG_IPV6" SERVER_WG_IPV6
 SERVER_PORT=1194
 read -rp "Server's WireGuard port " -e -i "$SERVER_PORT" SERVER_PORT
 
-CLIENT_WG_IPV4="${SERVER_WG_IPV4%.*}.2"
-read -rp "Client's WireGuard IPv4 " -e -i "$CLIENT_WG_IPV4" CLIENT_WG_IPV4
-
-CLIENT_WG_IPV6="${SERVER_WG_IPV6%:*}:2"
-read -rp "Client's WireGuard IPv6 " -e -i "$CLIENT_WG_IPV6" CLIENT_WG_IPV6
-
+CLIENT_AMOUNT=3
+read -rp "How many Clients do you want to Generate? " -e -i "$CLIENT_AMOUNT" CLIENT_AMOUNT
 # Retrieve system DNS for default
 CLIENT_DNS_1="$(grep ^nameserver /etc/resolv.conf | awk '{printf $2}')"
+
+if [[ $(grep ^nameserver /etc/resolv.conf | awk '{printf $2}') != 127.0.0.53 ]] && [[ $(grep ^nameserver /etc/resolv.conf | awk '{printf $2}') != "" ]]; then
+	CLIENT_DNS_1=$(grep ^nameserver /etc/resolv.conf | awk '{printf $2}')
+else
+	CLIENT_DNS_1=1.1.1.1
+fi
+
 read -rp "First DNS resolver to use for the client: " -e -i "$CLIENT_DNS_1" CLIENT_DNS_1
 
-CLIENT_DNS_2="$(grep ^nameserver /etc/resolv.conf | awk '{printf $3}')"
+if [[ $(grep ^nameserver /etc/resolv.conf | awk '{printf $3}') != 127.0.0.53 ]] && [[ $(grep ^nameserver /etc/resolv.conf | awk '{printf $3}') != "" ]]; then
+	CLIENT_DNS_2=$(grep ^nameserver /etc/resolv.conf | awk '{printf $3}')
+else
+	CLIENT_DNS_2=9.9.9.9
+fi
 read -rp "Second DNS resolver to use for the client: " -e -i "$CLIENT_DNS_2" CLIENT_DNS_2
 
 # Ask for pre-shared symmetric key
 IS_PRE_SYMM="y"
 read -rp "Want to use pre-shared symmetric key? [Y/n]: " -e -i "$IS_PRE_SYMM" IS_PRE_SYMM
+
 
 if [[ $SERVER_PUB_IP =~ .*:.* ]]
 then
@@ -84,39 +92,41 @@ else
   ENDPOINT="$SERVER_PUB_IP:$SERVER_PORT"
 fi
 
-# Install WireGuard tools and module
-if [[ "$OS" = 'ubuntu' ]]; then
-	apt install software-properties-common -y
-	add-apt-repository ppa:wireguard/wireguard -y
-	apt-get update
-	if [[ $go = 1 ]]; then
-		sudo add-apt-repository ppa:longsleep/golang-backports -y
-		sudo apt-get update
-		sudo apt-get install -y golang-go
-		apt-get -y install make git
-		cd /usr/local/src || mkdir -p /usr/local/src ; cd /usr/local/src 
-		git clone https://git.zx2c4.com/wireguard-go
-		cd wireguard-go
-		make
-		sudo cp wireguard-go /usr/local/bin
-		apt-get -y install wireguard-tools --no-install-recommends
-	else
-		apt-get -y install wireguard
+if [[ $(which wg-quick) = "" ]]; then
+	# Install WireGuard tools and module
+	if [[ "$OS" = 'ubuntu' ]]; then
+		apt install software-properties-common -y
+		add-apt-repository ppa:wireguard/wireguard -y
+		apt-get update
+		if [[ $go = 1 ]]; then
+			sudo add-apt-repository ppa:longsleep/golang-backports -y
+			sudo apt-get update
+			sudo apt-get install -y golang-go
+			apt-get -y install make git
+			cd /usr/local/src || mkdir -p /usr/local/src ; cd /usr/local/src
+			git clone https://git.zx2c4.com/wireguard-go
+			cd wireguard-go
+			make
+			sudo cp wireguard-go /usr/local/bin
+			apt-get -y install wireguard-tools --no-install-recommends
+		else
+			apt-get -y install wireguard
+		fi
+	elif [[ "$OS" = 'debian' ]]; then
+		echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
+		printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
+		apt update
+		apt install wireguard
+	elif [[ "$OS" = 'fedora' ]]; then
+		dnf copr enable jdoss/wireguard
+		dnf install wireguard-dkms wireguard-tools
+	elif [[ "$OS" = 'centos' ]]; then
+		curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+		yum install epel-release
+		yum install wireguard-dkms wireguard-tools
+	elif [[ "$OS" = 'arch' ]]; then
+		pacman -S wireguard-tools
 	fi
-elif [[ "$OS" = 'debian' ]]; then
-	echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
-	printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
-	apt update
-	apt install wireguard
-elif [[ "$OS" = 'fedora' ]]; then
-	dnf copr enable jdoss/wireguard
-	dnf install wireguard-dkms wireguard-tools
-elif [[ "$OS" = 'centos' ]]; then
-	curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
-	yum install epel-release
-	yum install wireguard-dkms wireguard-tools
-elif [[ "$OS" = 'arch' ]]; then
-	pacman -S wireguard-tools
 fi
 
 # Make sure the directory exists (this does not seem the be the case on fedora)
@@ -126,9 +136,16 @@ mkdir /etc/wireguard > /dev/null 2>&1
 SERVER_PRIV_KEY=$(wg genkey)
 SERVER_PUB_KEY=$(echo "$SERVER_PRIV_KEY" | wg pubkey)
 
-# Generate key pair for the server
-CLIENT_PRIV_KEY=$(wg genkey)
-CLIENT_PUB_KEY=$(echo "$CLIENT_PRIV_KEY" | wg pubkey)
+# Add pre shared symmetric key to respective files
+case "$IS_PRE_SYMM" in
+	[yY][eE][sS]|[yY])
+		CLIENT_SYMM_PRE_KEY=$( wg genpsk )
+		# echo "PresharedKey = $CLIENT_SYMM_PRE_KEY" >> "/etc/wireguard/$SERVER_WG_NIC.conf"
+		# echo "PresharedKey = $CLIENT_SYMM_PRE_KEY" >> "$HOME/$SERVER_WG_NIC-client-$post.conf"
+		;;
+esac
+
+
 
 # Add server interface
 echo "[Interface]
@@ -137,33 +154,51 @@ ListenPort = $SERVER_PORT
 PrivateKey = $SERVER_PRIV_KEY
 PostUp = iptables -t nat -A POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE; iptables -A FORWARD -i $SERVER_WG_NIC -j ACCEPT; iptables -A INPUT -p udp -m udp --dport $SERVER_PORT -j ACCEPT; ip6tables -A FORWARD -i $SERVER_WG_NIC -j ACCEPT; ip6tables -t nat -A POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE; ip6tables -A INPUT -p udp -m udp --dport $SERVER_PORT -j ACCEPT
 PostDown = iptables -t nat -D POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE; iptables -D FORWARD -i $SERVER_WG_NIC -j ACCEPT; iptables -D INPUT -p udp -m udp --dport $SERVER_PORT -j ACCEPT; ip6tables -D FORWARD -i $SERVER_WG_NIC -j ACCEPT; ip6tables -t nat -D POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE; ip6tables -D INPUT -p udp -m udp --dport $SERVER_PORT -j ACCEPT
+
 " > "/etc/wireguard/$SERVER_WG_NIC.conf"
 
-# Add the client as a peer to the server
-echo "[Peer]
+ipv6prefix=$(echo $SERVER_WG_IPV6 | sed -e 's+:.*,++' ) ; ipv6prefix="${ipv6prefix%:*}:"
+ipv4prefix=$(echo $SERVER_WG_IPV4| sed -e 's+\..*,++' ) ; ipv4prefix="${ipv4prefix%.*}."
+for (( count = 0; count < CLIENT_AMOUNT; count++ )); do
+	startnum=$(echo $SERVER_WG_IPV4 | sed -e 's+.*\.++')
+	post=$(( count + startnum + 1 ))
+	# Generate key pair for the server
+	CLIENT_PRIV_KEY=$(wg genkey)
+	CLIENT_PUB_KEY=$(echo "$CLIENT_PRIV_KEY" | wg pubkey)
+
+	CLIENT_WG_IPV4=$ipv4prefix$post
+	CLIENT_WG_IPV6=$ipv6prefix$post
+	# Add the client as a peer to the server
+	echo "[Peer]
 PublicKey = $CLIENT_PUB_KEY
 AllowedIPs = $CLIENT_WG_IPV4/32,$CLIENT_WG_IPV6/128" >> "/etc/wireguard/$SERVER_WG_NIC.conf"
+	if [[ $CLIENT_SYMM_PRE_KEY != "" ]]; then
+		echo "PresharedKey = $CLIENT_SYMM_PRE_KEY
+" >> "/etc/wireguard/$SERVER_WG_NIC.conf"
+	else
+		echo "
+" >> "/etc/wireguard/$SERVER_WG_NIC.conf"
+	fi
 
-# Create client file with interface
-echo "[Interface]
+	# Create client file with interface
+	echo "[Interface]
 PrivateKey = $CLIENT_PRIV_KEY
 Address = $CLIENT_WG_IPV4/24,$CLIENT_WG_IPV6/64
-DNS = $CLIENT_DNS_1,$CLIENT_DNS_2" > "$HOME/$SERVER_WG_NIC-client.conf"
-
-# Add the server as a peer to the client
-echo "[Peer]
+DNS = $CLIENT_DNS_1,$CLIENT_DNS_2
+" > "$HOME/$SERVER_WG_NIC-client-$post.conf"
+	# Add the server as a peer to the client
+	echo "[Peer]
 PublicKey = $SERVER_PUB_KEY
 Endpoint = $ENDPOINT
-AllowedIPs = 0.0.0.0/0,::/0" >> "$HOME/$SERVER_WG_NIC-client.conf"
-
-# Add pre shared symmetric key to respective files
-case "$IS_PRE_SYMM" in
-	[yY][eE][sS]|[yY])
-		CLIENT_SYMM_PRE_KEY=$( wg genpsk )
-		echo "PresharedKey = $CLIENT_SYMM_PRE_KEY" >> "/etc/wireguard/$SERVER_WG_NIC.conf"
-		echo "PresharedKey = $CLIENT_SYMM_PRE_KEY" >> "$HOME/$SERVER_WG_NIC-client.conf"
-		;;
-esac
+AllowedIPs = 0.0.0.0/0,::/0" >> "$HOME/$SERVER_WG_NIC-client-$post.conf"
+	if [[ $CLIENT_SYMM_PRE_KEY != "" ]]; then
+		echo "PresharedKey = $CLIENT_SYMM_PRE_KEY
+" >> "$HOME/$SERVER_WG_NIC-client-$post.conf"
+	else
+		echo "
+" >> "$HOME/$SERVER_WG_NIC-client-$post.conf"
+	fi
+done
 
 chmod 600 -R /etc/wireguard/
 
